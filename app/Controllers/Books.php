1b01f40bca686436feb16aaf0198d77adf5aabab
Sachin Borse <sachin.bookonspot@gmail.com>
2022-10-12 17:12:19 +0530

    Book_controller declare

---
+++ app/Controllers/Books.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controllers;
+
+use codeigniter\Controller;
+use CodeIgniter\HTTP\RequestInterface;
+use CodeIgniter\HTTP\ResponseInterface;
+use App\Models\Book_model;
+
+class Books extends BaseController
+{
+    public function index()
+    {
+        //return view('welcome_message');
+    }
+}
